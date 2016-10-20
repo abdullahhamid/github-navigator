c66f233cd83a692112266c8b80354aef418c24b9
Abdullah Hamid <abdullahroshan17@gmail.com>
2016-10-20 21:59:19 +0100

    templates and top 5 results added

--- application.py
+++ application.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 #Add index template here
 @app.route("/")
 def main():
-    return "Welcome to GitHub Navigator!"
+    return render_template('index.html')
 
 
 @app.route("/navigator")
@@ -21,33 +21,40 @@ def navigator():
         return "please use search_term=arrow"
     search_results = get_search_results(search_term)
     sorted_list = get_sorted_list(search_results)
-    #app.logger.info(search_results)
-    return render_template('template.html', created_at=sorted_list)
-
-#also add for 404
-#@app.errorhandler(500)
-#def internal_server_error(error):
-#    app.logger.error('Server Error: %s', (error))
-#    return render_template('500.htm'), 500
+    app.logger.info(sorted_list)
+    return render_template('template.html', sorted_list=sorted_list, search_term=search_term)
 
 
-#@app.errorhandler(Exception)
-#def unhandled_exception(e):
-#    app.logger.error('Unhandled Exception: %s', (e))
-#    return render_template('500.htm'), 500
+@app.errorhandler(404)
+def internal_server_error(error):
+    app.logger.error('404 Not Found')
+    return render_template('404.html'), 404
 
 
+@app.errorhandler(500)
+def internal_server_error(error):
+    app.logger.error('Server Error: %s', (error))
+    return render_template('500.html'), 500
+
+
+@app.errorhandler(Exception)
+def unhandled_exception(e):
+    app.logger.error('Unhandled Exception: %s', (e))
+    return render_template('500.htm'), 500
+
+
+# get top five sorted search results
 def get_sorted_list(results):
     sorted_list = sorted(results["items"], key=lambda k: k.get("created_at", 0), reverse=True)
-    for entry in sorted_list:
-        app.logger.info(entry["created_at"])
-    return "hello"
+    top_five = []
+    for i in range(5):
+        top_five.append(sorted_list[i])
+    return top_five
 
 
 def get_search_results(search_term):
     search_results = requests.get("https://api.github.com/search/repositories?q=" + search_term)
     json_data = json.loads(search_results.content)
-    #str = json_data["items"][0]["created_at"]
     return json_data
 
 
