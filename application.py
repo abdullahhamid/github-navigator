adf3b16e3ea270d28b8a0a1235bdd11767f9563f
Abdullah Hamid <abdullahroshan17@gmail.com>
2016-10-23 00:21:21 +0100

    info about latest commits added, unittests and templates updated

--- application.py
+++ application.py
@@ -2,61 +2,68 @@
 
 import json
 import requests
-import logging
 
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-#Add index template here
+
+# get the latest commits of repository - inputs: repository owner login, repository name
+def get_latest_commit(repo_owner, repo_name):
+    search_results = requests.get("https://api.github.com/repos/" + repo_owner + "/" + repo_name + "/commits")
+    json_data = json.loads(search_results.content)
+    return json_data
+
+
+# get top five sorted search results
+def get_sorted_search_results(search_term):
+    search_results = requests.get("https://api.github.com/search/repositories?q=" + search_term)
+    json_data = json.loads(search_results.content)
+    # sort the search results
+    sorted_list = sorted(json_data["items"], key=lambda k: k.get("created_at", 0), reverse=True)
+    top_five = []
+    # get latest commits for the top 5 repositories in the results
+    for i in range(len(sorted_list)):
+        if i == 5:
+            break
+        top_five.append(sorted_list[i])
+        top_five[i]["commit_details"] = get_latest_commit(sorted_list[i]["owner"]["login"], sorted_list[i]["name"])
+    return top_five
+
+
+# Home page of the application
 @app.route("/")
 def main():
     return render_template('index.html')
 
 
+# navigator route
 @app.route("/navigator")
 def navigator():
     search_term = request.args.get("search_term")
     if search_term is None:
-        return "please use search_term=arrow"
-    search_results = get_search_results(search_term)
-    sorted_list = get_sorted_list(search_results)
-    app.logger.info(sorted_list)
-    return render_template('template.html', sorted_list=sorted_list, search_term=search_term)
+        return render_template('search.html')  # send back the correct usage page
+    sorted_search_results = get_sorted_search_results(search_term)
+    return render_template('template.html', sorted_list=sorted_search_results, search_term=search_term)
 
 
 @app.errorhandler(404)
-def internal_server_error(error):
+def page_not_found(error):
     app.logger.error('404 Not Found')
     return render_template('404.html'), 404
 
 
 @app.errorhandler(500)
 def internal_server_error(error):
-    app.logger.error('Server Error: %s', (error))
+    app.logger.error('Server Error: %s', error)
     return render_template('500.html'), 500
 
 
 @app.errorhandler(Exception)
 def unhandled_exception(e):
-    app.logger.error('Unhandled Exception: %s', (e))
+    app.logger.error('Unhandled Exception: %s', e)
     return render_template('500.html'), 500
 
 
-# get top five sorted search results
-def get_sorted_list(results):
-    sorted_list = sorted(results["items"], key=lambda k: k.get("created_at", 0), reverse=True)
-    top_five = []
-    for i in range(5):
-        top_five.append(sorted_list[i])
-    return top_five
-
-
-def get_search_results(search_term):
-    search_results = requests.get("https://api.github.com/search/repositories?q=" + search_term)
-    json_data = json.loads(search_results.content)
-    return json_data
-
-
 if __name__ == "__main__":
-    app.run(debug=True)  # Port changed to 9876 instead of default 5000
+    app.run()  # Default: http://127.0.0.1:5000/
--- application_test.py
+++ application_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from application import app
+from application import app, get_sorted_search_results, get_latest_commit
 
 import unittest
 
@@ -15,6 +15,16 @@ class ApplicationTestCases(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get('/navigator?search_term=arrow')
         self.assertEqual(response.status_code, 200)
+
+    def test_get_sorted_search_results(self):
+        json_obj = get_sorted_search_results("arrow")
+        self.assertIsNotNone(json_obj)
+        self.assertEqual(json_obj.__len__(), 5)
+
+    def test_get_latest_commit(self):
+        json_obj = get_latest_commit("abdullahhamid", "github-navigator")
+        self.assertIsNotNone(json_obj)
+        self.assertGreater(json_obj.__len__(), 1)
 
     def test_404(self):
         tester = app.test_client(self)
